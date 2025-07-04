9c8f9780c880eceb5fca727634b78c205cb68ed8
Ramakrishna-K <ramakrishnakatravth0@gmail.com>
2025-07-04 21:45:34 +0530

    casting in python

---
+++ python2_casting.py
@@ -0,0 +1,46 @@
+# Casting (or type conversion) in Python means converting a variable from one data type to another — 
+# for example, converting a string to an integer or a float to a string.
+
+# convert int
+
+
+
+# x = "10"
+# y = int(x)   # Now y is an integer
+# print(y + 5)  # Output: 15
+
+
+# convert float
+
+# x = "3.42"
+# y = 34
+# z = float(x)
+# c = float(y)
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# print(type(c))
+
+# print(z)
+# print(c)
+
+# convert into string
+
+# x = 100
+
+# y = str(x)
+
+# print(y + " is a number")
+
+# #  Convert to bool
+# print(bool(0))     # False
+# print(bool(5))     # True
+# print(bool(""))    # False
+# print(bool("Hi"))  # True
+
+#  Convert to list
+
+x = "hello"
+y = list(x)
+print(y)  # Output: ['h', 'e', 'l', 'l', 'o']
